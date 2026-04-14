086104bbaaae8b1227644a1c701638d9b136adc3
Fenglin Yu (MikukuOvO) <fengliny@andrew.cmu.edu>
2026-04-14 17:23:20 +0000

    feat: add diffusion tensor weight checker mixin path
    Co-authored-by: MikukuOvO <118185781+MikukuOvO@users.noreply.github.com>

--- python/sglang/multimodal_gen/runtime/managers/scheduler.py
+++ python/sglang/multimodal_gen/runtime/managers/scheduler.py
@@ -20,6 +20,7 @@ from sglang.multimodal_gen.runtime.entrypoints.post_training.io_struct import (
     ReleaseMemoryOccupationReqInput,
     ResumeMemoryOccupationReqInput,
     UpdateWeightFromDiskReqInput,
+    UpdateWeightFromTensorCheckerReqInput,
     UpdateWeightFromTensorReqInput,
 )
 from sglang.multimodal_gen.runtime.entrypoints.utils import (
@@ -103,6 +104,9 @@ class Scheduler(SchedulerPostTrainingMixin):
             ShutdownReq: self._handle_shutdown,
             UpdateWeightFromDiskReqInput: self._handle_update_weights_from_disk,
             UpdateWeightFromTensorReqInput: self._handle_update_weights_from_tensor,
+            UpdateWeightFromTensorCheckerReqInput: (
+                self._handle_update_weights_from_tensor_checker
+            ),
             GetWeightsChecksumReqInput: self._handle_get_weights_checksum,
             ReleaseMemoryOccupationReqInput: self._handle_release_memory_occupation,
             ResumeMemoryOccupationReqInput: self._handle_resume_memory_occupation,
